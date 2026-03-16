1c80faa183591cd21f54055f83894fa09cb687c0
EliasUsuario <correoelias84@gmail.com>
2026-03-16 18:52:03 +0100

    hasta el ejercicio 22

--- ejercicios.py
+++ ejercicios.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 # 1- Escribe una función que reciba una cadena de texto como parámetro y devuelva un diccionario con las frecuencias de cada letra en la cadena. 
     # Los espacios no deben ser considerados.
 def contar_frecuencias(cadena):
@@ -263,12 +266,138 @@ resultado = filtrar_por_inicial(animales, letra)
 print(f"Lista original: {animales}")
 print(f"Palabras que empiezan por '{letra}': {resultado}")
 
+
+#15- Crea una función lambda que sume 3 a cada número de una lista dada.
+numeros = [10, 20, 30, 40, 50]
+resultado_objeto = map(lambda x: x + 3, numeros)# Uso map() junto con una función lambda
+resultado_lista = list(resultado_objeto)# Convierto el resultado a lista para poder visualizarlo
+
+print(f"Lista original: {numeros}")
+print(f"Lista tras sumar 3: {resultado_lista}")
+
+#16- Escribe una función que tome una cadena de texto y un número entero n como parámetros y devuelva una lista de todas las palabras que sean más largas que n. Usa la función filter().
+def filtrar_palabras_largas(texto, n):
+
+    palabras = texto.split()# Conversion del texto en una lista de palabras.
+    def es_larga(palabra): # Solo se devuelve True si la longitud de la palabra supera n
+        return len(palabra) > n
+
+    resultado_iterador = filter(es_larga, palabras)
+    return list(resultado_iterador)
+
+# Ejemplo de prueba
+frase_ejemplo = "Esta es una frase que he puesto de ejemplo"
+longitud_minima = 4
+
+resultado = filtrar_palabras_largas(frase_ejemplo, longitud_minima)
+
+print(f"Texto original: '{frase_ejemplo}'")
+print(f"Palabras con más de {longitud_minima} caracteres: {resultado}")
+
+#17- Crea una función que tome una lista de dígitos y devuelva el número correspondiente. 
+    # (Por ejemplo, [5,7,2] corresponde al número 572). Usa la función reduce().
+
+def lista_a_numero(digitos):
+    def combinar(acumulado, siguiente): # Se define la función con "acumulado" (lo calculado hasta ahora) y "siguiente" (el nuevo dígito que vamos a procesar)
+        return (acumulado * 10) + siguiente # Desplazamos el acumulado una posición a la izquierda (x10) y sumamos el nuevo dígito.
+    resultado = reduce(combinar, digitos) # Aplicamos reduce para "reducir" la lista a un solo valor
+    return resultado
+
+# Pruebas de funcionamiento
+mi_lista = [5, 7, 2]
+numero_final = lista_a_numero(mi_lista)
+
+print(f"Lista original: {mi_lista}")
+print(f"Número resultante: {numero_final}")
+
+#18- Escribe un programa en Python que cree una lista de diccionarios con información de estudiantes (nombre, edad, calificación).
+    # y use filter para extraer a los estudiantes con una calificación mayor o igual a 90.
+
+def filtrar_estudiantes(lista_estudiantes):
+    def calificacion_alta(estudiante):
+        return estudiante['calificación'] >= 90 # Acceso al valor de la clave 'calificación' del diccionario y comprobación de si cumple la condición.
+    estudiantes_filtrados = filter(calificacion_alta, lista_estudiantes)# Uso de filter() para aplicar la regla a cada diccionario de la lista.
+
+    
+    return list(estudiantes_filtrados)# Convierto el resultado a una lista para poder trabajar con ella.
+
+# Datos de prueba con una lista de diccionarios:
+estudiantes = [
+    {"nombre": "Elías", "edad": 42, "calificación": 95},
+    {"nombre": "Pepe", "edad": 32, "calificación": 88},
+    {"nombre": "Yomismo", "edad": 44, "calificación": 92},
+    {"nombre": "Juanito", "edad": 21, "calificación": 75},
+    {"nombre": "Susan", "edad": 20, "calificación": 90}
+]
+
+# Ejecución de la función
+mejores_estudiantes = filtrar_estudiantes(estudiantes)
+
+print("Lista de Estudiantes con calificación mayor o igual a 90")
+for e in mejores_estudiantes:
+    print(f"Nombre: {e['nombre']}, Calificación: {e['calificación']}")
+
+
+#19- Crea una función lambda que filtre los números impares de una lista dada.
+numeros_variados = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] # Lista de números inicial para la prueba
+
+impares = filter(lambda x: x % 2 != 0, numeros_variados) # La lambda recibe 'x' y devuelve True si el resto de x/2 no es 0 (1 = True).
+resultado_lista = list(impares) # Convierto el resultado a lista para poder imprimirlo
+
+print(f"Lista completa: {numeros_variados}")
+print(f"Números impares: {resultado_lista}")
+
+#20- Para una lista con elementos de tipo integer y string, obtén una nueva lista solo con los valores int. Usa la función filter().
+def filtrar_enteros(lista_mixta):
+    def es_entero(elemento):# Función de comprobación
+        return type(elemento) == int # Comparo el tipo del elemento 'int' con type(así obtengo el tipo del elemento).
+    solo_enteros = filter(es_entero, lista_mixta)
+    return list(solo_enteros)
+
+# Datos de prueba con mezclas
+datos = [10, "Prueba", 5, "Hola", 22, "30", True]
+
+# Ejecución
+resultado = filtrar_enteros(datos)
+
+print(f"Lista original: {datos}")
+print(f"Solo valores enteros: {resultado}")
+
+#21- Crea una función que calcule el cubo de un número dado mediante una función lambda.
+calcular_cubo = lambda x: x ** 3 # x**3 es equivalente a x * x * x
+
+# Datos de prueba
+numero = 4
+resultado = calcular_cubo(numero)
+print(f"El cubo de {numero} es: {resultado}")
+
+#22- Dada una lista numérica, obtén el producto total de los valores. Usa la función reduce().
+def calcular_producto_total(numeros):
+    producto = reduce(lambda x, y: x * y, numeros)# 'x' es el acumulado (se multiplica por el siguiente), 'y' es el valor actual de la lista
+    return producto
+
+# Datos de prueba
+mi_lista = [2, 3, 4, 5] # El resultado debería ser 120 (2*3*4*5)
+
+resultado = calcular_producto_total(mi_lista)
+
+print(f"Lista de números: {mi_lista}")
+print(f"El producto total es: {resultado}")
+
+#23- Concatena una lista de palabras. Usa la función reduce(). 
+
+
+
+
+
+
+
+
+
+
+
 '''
-Ejercicios comentados hasta aquí (para no tener que usar el input cada vez): 8, 11, 12
+Ejercicios comentados hasta aquí (para no tener que usar el input cada vez que lo ejecuto): 8, 11, 12
 '''
-
-
-
-
 
 
